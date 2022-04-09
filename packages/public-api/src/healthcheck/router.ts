3b7805f476073a5c11cb87f25f5a44b6683e8cbd
Konstantin Darutkin <spalt08@mail.ru>
2022-04-09 18:41:11 +0800

    wip: cd pipeline

--- packages/public-api/src/healthcheck/router.ts
+++ packages/public-api/src/healthcheck/router.ts
@@ -1,11 +1,10 @@
 import { Router } from 'express';
 import { respond } from '../middleware/response';
-import { version } from '../../package.json';
 
 const router = Router();
 
 router.get('/', (_, res) => {
-  respond(res, `gradejs-public-api ${version}`);
+  respond(res, `gradejs-public-api`);
 });
 
 export default router;
