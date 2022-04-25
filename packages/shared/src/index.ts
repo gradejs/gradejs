2a7de8059ec731f03821d9deb1989b05431d2a73
Konstantin Darutkin <spalt08@mail.ru>
2022-04-25 15:19:21 +0800

    wip

---
+++ packages/shared/src/index.ts
@@ -0,0 +1,8 @@
+export * from './database/connection';
+export * from './database/entities/webPage';
+export * from './database/entities/webPagePackage';
+
+export * from './internalApi/api';
+
+export * from './utils/env';
+export * from './utils/testUtils';
---
+++ packages/shared/src/internalApi/types.ts
@@ -0,0 +1,16 @@
+export interface WebsiteInternal {
+  id: number;
+  url: string;
+  status: WebsiteStatusInternal;
+  packages: string[];
+  updatedAt: string;
+  createdAt: string;
+}
+
+export enum WebsiteStatusInternal {
+  Created = 'created',
+  InProgress = 'in-progress',
+  Ready = 'ready',
+  Failed = 'failed',
+  Invalid = 'invalid',
+}
