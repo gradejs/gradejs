a983eccf9d4cd3813582ab158c04fc2f312d5e79
Konstantin Darutkin <spalt08@mail.ru>
2022-05-17 13:39:05 +0800

    fix: review fixes

--- packages/worker/src/tasks/syncPackageIndex.ts
+++ packages/worker/src/tasks/syncPackageIndex.ts
@@ -1,6 +1,8 @@
+import concurrently from 'tiny-async-pool';
 import { internalApi, queueWorkerTask } from '@gradejs-public/shared';
 
 const PKG_SYNC_BATCH_LIMIT = 50;
+const PKG_SYNC_PARALLEL_LIMIT = 10;
 
 /**
  * Retrieves data slices from the internal API and schedules batch worker tasks.
@@ -8,15 +10,21 @@ const PKG_SYNC_BATCH_LIMIT = 50;
  */
 export async function syncPackageIndex() {
   const { pagination } = await internalApi.fetchPackageIndex();
-  const promises: Promise<unknown>[] = [];
 
-  for (let offset = 0; offset < pagination.total; offset += PKG_SYNC_BATCH_LIMIT) {
-    promises.push(
-      internalApi
-        .fetchPackageIndex(offset, PKG_SYNC_BATCH_LIMIT)
-        .then(({ packages }) => queueWorkerTask('syncPackageIndexBatch', packages))
-    );
+  let processed = 0;
+  let batchesTotal = Math.ceil(pagination.total / PKG_SYNC_BATCH_LIMIT);
+  const offsets = new Array(batchesTotal).fill(0).map((_, i) => i * PKG_SYNC_BATCH_LIMIT);
+
+  async function processBatch(offset: number) {
+    const { packages } = await internalApi.fetchPackageIndex(offset, PKG_SYNC_BATCH_LIMIT);
+    await queueWorkerTask('syncPackageIndexBatch', packages);
+    processed++;
   }
 
-  await Promise.all(promises);
+  await concurrently(PKG_SYNC_PARALLEL_LIMIT, offsets, processBatch).catch((e: Error) =>
+    console.error(
+      `syncPackageIndex task is aborted, only ${processed} out of ${batchesTotal} batches are processed:` +
+        `\n${e.message}\n${e.stack}`
+    )
+  );
 }
