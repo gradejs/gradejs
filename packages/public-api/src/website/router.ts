54d410de3528c505f47db22f97f5dd17cbc9a1ce
Konstantin Darutkin <spalt08@mail.ru>
2022-08-17 18:08:30 +0300

    feat: typed router proposal

--- packages/public-api/src/website/router.ts
+++ packages/public-api/src/website/router.ts
@@ -1,8 +1,4 @@
-import { Router } from 'express';
-import j from 'joi';
-import { wrapTryCatch } from '../middleware/common';
-import { NotFoundError, respond } from '../middleware/response';
-import { parseBody, parseSlug } from '../middleware/validation';
+import { NotFoundError } from '../middleware/response';
 import {
   getPackagesByHostname,
   getWebPagesByHostname,
@@ -10,39 +6,33 @@ import {
   syncWebPage,
 } from './service';
 import { getAffectingVulnerabilities } from '../vulnerabilities/vulnerabilities';
+import { TypedRouter } from '../api';
 
-const router = Router();
+const router = new TypedRouter();
 
-router.get(
-  '/website/:hostname',
-  wrapTryCatch(async (req, res) => {
-    const hostname = parseSlug(req, 'hostname', j.string().required().hostname());
-    const webpages = await getWebPagesByHostname(hostname);
+router.get('/website/:hostname', async (req) => {
+  const { hostname } = req.params;
+  const webpages = await getWebPagesByHostname(hostname);
 
-    if (webpages.length === 0) {
-      throw new NotFoundError();
-    }
+  if (webpages.length === 0) {
+    throw new NotFoundError();
+  }
 
-    // Sync webpages if status is not processed
-    await Promise.all(webpages.map((webpage) => syncWebPage(webpage)));
+  // Sync webpages if status is not processed
+  await Promise.all(webpages.map((webpage) => syncWebPage(webpage)));
 
-    const packages = await getPackagesByHostname(hostname);
-    const vulnerabilities = await getAffectingVulnerabilities(packages);
+  const packages = await getPackagesByHostname(hostname);
+  const vulnerabilities = await getAffectingVulnerabilities(packages);
 
-    respond(res, { webpages, packages, vulnerabilities });
-  })
-);
+  return { webpages, packages, vulnerabilities };
+});
 
-router.post(
-  '/webpage',
-  wrapTryCatch(async (req, res) => {
-    const urlOptions = { scheme: ['http', 'https'] };
-    const url = parseBody(req, 'url', j.string().required().uri(urlOptions));
+router.post('/webpage', async (req) => {
+  const { url } = req.body;
 
-    const webpage = await requestWebPageParse(url);
+  const webpage = await requestWebPageParse(url);
 
-    respond(res, webpage);
-  })
-);
+  return webpage;
+});
 
-export default router;
+export default router.expressRouter;
