586e1c19510b850d6b7783f2fc069591d83f4e37
Dmitry Shakun <smilecraft1995@gmail.com>
2022-09-15 00:12:48 +0300

    fix: wrap card list skeletons in grid

--- packages/web/src/components/ui/CardList/CardListSkeleton.tsx
+++ packages/web/src/components/ui/CardList/CardListSkeleton.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import styles from './CardList.module.scss';
 import { repeat } from '../../../utils/helpers';
 import Skeleton from '../Skeleton/Skeleton';
 import Card from '../Card/Card';
 
 export const CardListSkeleton = () => (
-  <>
+  <div className={styles.grid}>
     {repeat(
       3,
       <Skeleton width='100%' variant='rounded'>
         <Card id='id1' title='title' />
       </Skeleton>
     )}
-  </>
+  </div>
 );
