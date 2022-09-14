f482f0223bb79d5601646477d8e51e59c5eca9ba
Dmitry Shakun <smilecraft1995@gmail.com>
2022-09-15 00:02:48 +0300

    fix: remove image placeholder prop for keywords list

--- packages/web/src/components/ui/SearchResultsSidebar/SearchResultsSidebar.tsx
+++ packages/web/src/components/ui/SearchResultsSidebar/SearchResultsSidebar.tsx
@@ -100,7 +100,6 @@ export default function SearchResultsSidebar({
           selectHandler={handleKeywordsChange}
           returnButton={() => setModalKeywordsOpen(false)}
           resetGroup={() => setSelectedKeywords([])}
-          itemsWithImage
           searchOpen
         />
       </Modal>
