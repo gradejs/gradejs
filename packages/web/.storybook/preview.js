ca2f8e874b15f1e3996274951ba19a7d2fa44400
Konstantin Darutkin <spalt08@mail.ru>
2022-09-27 17:45:27 +0400

    fix: storybook (#68)

--- packages/web/.storybook/preview.js
+++ packages/web/.storybook/preview.js
@@ -1,4 +1,12 @@
+import { addDecorator } from '@storybook/react';
+import { MemoryRouter } from 'react-router';
+import { Provider } from 'react-redux';
+import { store } from '../src/store';
 import '../src/styles/global.scss';
+
+// Some elements, like <Link> may not work without a router context
+addDecorator((story) => <MemoryRouter>{story()}</MemoryRouter>);
+addDecorator((story) => <Provider store={store}>{story()}</Provider>);
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
