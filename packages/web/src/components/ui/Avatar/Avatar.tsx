8670c1790a6d411a6ef8a3ca349335129f0625fe
Oleg Klimenko <me@ctizen.dev>
2022-09-30 13:45:43 +0300

    fix: make authors grouped avatars (#72)

--- packages/web/src/components/ui/Avatar/Avatar.tsx
+++ packages/web/src/components/ui/Avatar/Avatar.tsx
@@ -1,15 +1,36 @@
 import React from 'react';
 import styles from './Avatar.module.scss';
+import stylesTooltip from '../Tooltip/Tooltip.module.scss'; // TODO: is it ok to reference external styles?
+import { Config, usePopperTooltip } from 'react-popper-tooltip';
 
 type Props = {
   src: string;
+  altAsTooltip?: boolean;
   alt?: string;
 };
 
-const Avatar = ({ src, alt = '' }: Props) => (
-  <div className={styles.avatarItem}>
-    <img src={src} className={styles.avatarImage} alt={alt} />
-  </div>
-);
+const tooltipOptions: Config = { placement: 'top-end', offset: [20, 13] };
+
+const Avatar = ({ src, altAsTooltip = false, alt = '' }: Props) => {
+  const { getArrowProps, getTooltipProps, setTooltipRef, setTriggerRef, visible } =
+    usePopperTooltip(tooltipOptions);
+
+  return (
+    <>
+      <div className={styles.avatarItem} ref={setTriggerRef}>
+        <img src={src} className={styles.avatarImage} alt={alt} />
+      </div>
+      {altAsTooltip && visible && (
+        <div
+          ref={setTooltipRef}
+          {...getTooltipProps({ className: stylesTooltip.tooltipContainer })}
+        >
+          {alt}
+          <div {...getArrowProps({ className: stylesTooltip.tooltipArrow })} />
+        </div>
+      )}
+    </>
+  );
+};
 
 export default Avatar;
