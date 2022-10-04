f36e0a18c0cab27d2123cdfdc1c62dc030b009fd
Dmitry Shakun <smilecraft1995@gmail.com>
2022-10-04 14:08:16 +0300

    feat: limit maximum avatar items in avatar group  (#83)
    * feat: limit maximum avatar items in avatar group and show rest elements as extra avatar with number of items beyond current maximum
    
    * feat: add native lazy loading for avatar images, show author name besides image if only one maintainer present, set max avatars to 4, simplify AvatarGroup total calculation

--- packages/web/src/components/ui/AvatarGroup/AvatarGroup.stories.tsx
+++ packages/web/src/components/ui/AvatarGroup/AvatarGroup.stories.tsx
@@ -20,3 +20,16 @@ export const Primary: ComponentStory<typeof AvatarGroup> = () => (
     </AvatarGroup>
   </div>
 );
+
+export const MoreThanMax: ComponentStory<typeof AvatarGroup> = () => (
+  <div style={{ display: 'flex', justifyContent: 'center' }}>
+    <AvatarGroup max={4}>
+      <Avatar src='https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg' />
+      <Avatar src='https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg' />
+      <Avatar src='https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg' />
+      <Avatar src='https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg' />
+      <Avatar src='https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg' />
+      <Avatar src='https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg' />
+    </AvatarGroup>
+  </div>
+);
