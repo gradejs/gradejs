3c9724543bb6d2730c2ca6cad62ba1234d1a52b3
Oleg Klimenko <me@ctizen.dev>
2022-10-19 18:00:33 +0300

    feat: add helmet for package page

--- packages/web/src/components/pages/Package.tsx
+++ packages/web/src/components/pages/Package.tsx
@@ -3,6 +3,7 @@ import { Error } from 'components/layouts';
 import PackagePage from '../layouts/PackagePage/PackagePage';
 import { useParams } from 'react-router-dom';
 import { usePackageInfo } from '../../store/hooks/usePackageInfo';
+import { Helmet } from 'react-helmet';
 
 export function PackageView() {
   const { '*': packageName } = useParams();
@@ -17,5 +18,21 @@ export function PackageView() {
     return <Error host={packageName} message='Package is not found in the database' />;
   }
 
-  return <PackagePage loading={isLoading} packageInfo={packageInfo} />;
+  const title = `Real world usage & stats of ${packageName} - GradeJS`;
+  const description =
+    `GradeJS has discovered ${packageInfo?.usage.length} websites using ${packageName}` +
+    (packageInfo?.usage.length
+      ? `: ${packageInfo?.usage.slice(0, 4).map((u) => u.hostname?.hostname)} and more.`
+      : '');
+  return (
+    <>
+      <Helmet>
+        <title>{title}</title>
+        <meta name='description' content={description} />
+        <meta property='og:title' content={title} />
+        <meta property='og:description' content={description} />
+      </Helmet>
+      <PackagePage loading={isLoading} packageInfo={packageInfo} />
+    </>
+  );
 }
